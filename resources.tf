eb432fc2b3d02929f3cf534f56ea8d6cc1090639
Anton Marin <antonaryo85@gmail.com>
2020-05-19 22:56:48 +0300

    ssh

--- resources.tf
+++ resources.tf
@@ -20,13 +20,14 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_firewall" "vpc_network_ssh" {
   name    = "ssh-access"
   network = google_compute_network.vpc_network.name
+  enable_logging = true
 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
 
-  source_tags = ["ssh-server"]
+  target_tags = ["ssh-server"]
 }
 
 resource "google_compute_address" "vm_static_ip" {
