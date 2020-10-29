45a8d11c1aec74df64820f920a8105fe33650cd2
Anton Marin <antonaryo85@gmail.com>
2020-10-29 21:29:08 +0300

    single quotes invalid

--- resources.tf
+++ resources.tf
@@ -45,7 +45,7 @@ resource "google_compute_instance" "vm_instance" {
 
   provisioner "remote-exec" {
     inline = [
-      'docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "/home:/home" docker/compose:1.27.4 -f /home/provisioner/ingress/docker-compose.yml up -d',
+      "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v '/home:/home' docker/compose:1.27.4 -f /home/provisioner/ingress/docker-compose.yml up -d",
     ]
   }
 }
