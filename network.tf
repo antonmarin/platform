dad8d51f38f3a77fae29391ec2ed8a2f967b5263
Anton Marin <antonaryo85@gmail.com>
2020-10-29 21:18:04 +0300

    open http connections

--- network.tf
+++ network.tf
@@ -17,6 +17,42 @@ resource "google_compute_firewall" "vpc_network_ssh" {
   target_tags = ["ssh-server"]
 }
 
+resource "google_compute_firewall" "vpc_port_http" {
+  name    = "http-server"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  target_tags = ["http-server"]
+}
+resource "google_compute_firewall" "vpc_port_https" {
+  name    = "https-server"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  target_tags = ["https-server"]
+}
+
 resource "google_compute_address" "vm_static_ip" {
   name = "terraform-static-ip"
+}
+
+
+resource "google_compute_firewall" "vpc_port_traefik_ui" {
+  name    = "traefik-ui"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+
+  target_tags = ["traefik-ui"]
 }
