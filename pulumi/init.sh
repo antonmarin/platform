ebe8273c15520d491e44647f414a6f66e1a892a0
antonmarin <antonaryo@yandex.ru>
2026-04-04 16:41:13 +0300

    output resulting tunnel
    also removes vm

--- pulumi/init.sh
+++ pulumi/init.sh
@@ -5,15 +5,8 @@ brew install pulumi/tap/pulumi
 export PULUMI_BACKEND_URL=file://~/.pulumi
 pulumi login $PULUMI_BACKEND_URL
 
-
 ## 1. Login locally (no cloud)
 #pulumi login s3://my-pulumi-state-bucket
-## 2. Initialize stack
-#pulumi stack init fornex
-## 3. Set configuration
-#cat ~/.ssh/id_rsa | pulumi config set --secret sshPrivateKey
-# 3.1. set key password if required
-#pulumi config set --secret sshPrivateKeyPass
 ## 4. Preview
 #pulumi preview
 ## 5. Deploy
